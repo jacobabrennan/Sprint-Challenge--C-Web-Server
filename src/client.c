4e15f758a7ee127385c576b8176578029bcb002f
JacobABrennan <jacobabrennan@gmail.com>
2019-04-12 13:02:05 -0400

    Implement parse_url()

--- src/client.c
+++ src/client.c
@@ -27,29 +27,31 @@ typedef struct urlinfo_t {
 */
 urlinfo_t *parse_url(char *url)
 {
-  // copy the input URL so as not to mutate the original
-  char *hostname = strdup(url);
-  char *port;
-  char *path;
-
-  urlinfo_t *urlinfo = malloc(sizeof(urlinfo_t));
-
-  /*
-    We can parse the input URL by doing the following:
-
-    1. Use strchr to find the first backslash in the URL (this is assuming there is no http:// or https:// in the URL).
-    2. Set the path pointer to 1 character after the spot returned by strchr.
-    3. Overwrite the backslash with a '\0' so that we are no longer considering anything after the backslash.
-    4. Use strchr to find the first colon in the URL.
-    5. Set the port pointer to 1 character after the spot returned by strchr.
-    6. Overwrite the colon with a '\0' so that we are just left with the hostname.
-  */
-
-  ///////////////////
-  // IMPLEMENT ME! //
-  ///////////////////
-
-  return urlinfo;
+    // Allocate Memory
+    urlinfo_t *urlinfo = malloc(sizeof(urlinfo_t));
+    urlinfo->hostname = malloc(1024);
+    urlinfo->port = malloc(16);
+    urlinfo->path = malloc(1024);
+    // Strip the protocol
+    char *protocol_signifier = strstr(url, "://");
+    if(NULL != protocol_signifier)
+    {
+        url = protocol_signifier + 3;
+    }
+    // Determine position of port and path, handle errors
+    char *index_port = strchr(url, ':');
+    char *index_path = strchr(index_port, '/');
+    if(NULL == index_port || NULL == index_path)
+    {
+        printf("Urls must include both a port and a path");
+        exit(1);
+    }
+    // Populate struct from string
+    strncpy(urlinfo->hostname, url, index_port - url);
+    strncpy(urlinfo->port, index_port+1, (index_path - index_port) -1);
+    urlinfo->path = strdup(index_path+1);
+    // Return resultant struct
+    return urlinfo;
 }
 
 /**
@@ -77,25 +79,24 @@ int send_request(int fd, char *hostname, char *port, char *path)
 
 int main(int argc, char *argv[])
 {  
-  int sockfd, numbytes;  
-  char buf[BUFSIZE];
+    int sockfd, numbytes;  
+    char buf[BUFSIZE];
 
-  if (argc != 2) {
-    fprintf(stderr,"usage: client HOSTNAME:PORT/PATH\n");
-    exit(1);
-  }
+    if (argc != 2) {
+        fprintf(stderr,"usage: client HOSTNAME:PORT/PATH\n");
+        exit(1);
+    }
 
-  /*
-    1. Parse the input URL
-    2. Initialize a socket by calling the `get_socket` function from lib.c
-    3. Call `send_request` to construct the request and send it
-    4. Call `recv` in a loop until there is no more data to receive from the server. Print the received response to stdout.
-    5. Clean up any allocated memory and open file descriptors.
-  */
+    // Parse the input URL
+    parse_url(argv[1]);
+    // Initialize a socket by calling the `get_socket` function from lib.c
+    // Call `send_request` to construct the request and send it
+    // Call `recv` in a loop until there is no more data to receive from the server. Print the received response to stdout.
+    // Clean up any allocated memory and open file descriptors.
 
-  ///////////////////
-  // IMPLEMENT ME! //
-  ///////////////////
+    ///////////////////
+    // IMPLEMENT ME! //
+    ///////////////////
 
-  return 0;
+    return 0;
 }
